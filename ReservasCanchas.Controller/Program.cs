0cbeb8319ee75d71b4b05bb369f8386f61185e8b
Facundo Cipresso <facucipresso@gmail.com>
2025-11-07 10:50:54 -0300

    Adding conection to the Data Base

--- ReservasCanchas.Controller/Program.cs
+++ ReservasCanchas.Controller/Program.cs
@@ -1,22 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ReservasCanchas.DataAccess.Persistance;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// registro DbContext con Postgre
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
